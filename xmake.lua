e21ffb2398ecb55be605ad9a423dfe23a2b9a85b
Onish Sharma <neharora23587@gmail.com>
2026-06-14 14:30:03 +0530

    main: build: use latest version of opencv.
    Signed-off-by: Onish Sharma <neharora23587@gmail.com>

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,6 @@ set_languages("c++17")
 
 add_rules("mode.release", "mode.debug")
 
--- Use your pre-compiled Arch Linux system package
 add_requires("opencv", { system = true })
 
 target("door_opener")
@@ -12,20 +11,16 @@ set_kind("binary")
 add_files("src/*.cpp")
 add_packages("opencv")
 
--- Post-build: Copy the real .so files and ALL their matching symlinks to the build cache
 after_build(function(target)
 	local pkg = target:pkg("opencv")
 	if pkg then
 		for _, libfile in ipairs(pkg:get("libfiles")) do
-			-- Get the parent directory (usually /usr/lib) and filename
 			local libdir = path.directory(libfile)
 			local filename = path.filename(libfile)
 
-			-- Extract the base name (e.g., "libopencv_core") from "libopencv_core.so.4.13.0"
 			local basename = filename:match("^(libopencv_[^%.]+)")
 
 			if basename then
-				-- Find every symlink and real file matching 'libopencv_whatever.so*' in /usr/lib
 				for _, matched_file in ipairs(os.files(path.join(libdir, basename .. ".so*"))) do
 					os.cp(matched_file, target:targetdir())
 				end
